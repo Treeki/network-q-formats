3947d70299ffbf28b9c5cd9a67728638742aa96d
Lana-chan <erin.manal@gmail.com>
2015-09-12 03:33:05 -0300

    make script commandline-friendly

--- BUN/bunpack.py
+++ BUN/bunpack.py
@@ -1,20 +1,5 @@
-import sys, getopt
-import struct
+import struct, sys
 from PIL import Image
-
-with open('FILES/GFX/FONTW33.BUN', 'rb') as f:
-	data = f.read()
-with open('FILES/GFX/MAIN.PAL', 'rb') as f:
-	pal_data = f.read()
-
-# read the file header (list of offsets)
-first_offset = struct.unpack_from('<I', data, 0)[0]
-image_count = first_offset // 4
-offsets = []
-
-for i in range(image_count):
-	offsets.append(struct.unpack_from('<I', data, i * 4)[0])
-
 
 def extract_image(data, offset, pal):
 	width = data[offset + 2]
@@ -51,9 +36,58 @@ def extract_image(data, offset, pal):
 
 	return img
 
+def main(argv):
+  import argparse, string
+  import os.path
+  
+  parser = argparse.ArgumentParser(description='Converts Network Q .BUN files into .PNG')
+  parser.add_argument('input', metavar='infile', type=str, nargs=1, help='the input file (.BUN)')
+  parser.add_argument('-p', '--pal', type=str, help='optional palette file (.PAL)')
+  args = parser.parse_args()
+  
+  path,ext = os.path.splitext(args.input[0])
+  try:
+    palpath = args.input[1]
+  except:
+    palpath = path
+  
+  if ext != '.BUN':
+    print 'File does not have .BUN extension!'
+    return
+  
+  filename = os.path.split(path)
+  
+  try:
+    f = open(path + '.BUN', 'rb')
+  except IOError as e:
+    print 'Unable to open BUN file!'
+    return
+  else:
+    data = f.read()
+    f.close()
+  
+  try:
+    f = open(palpath + '.PAL', 'rb')
+  except IOError as e:
+    print 'Unable to open PAL file!'
+    return
+  else:
+    pal_data = f.read()
+    f.close()
 
-for i, offset in enumerate(offsets):
-	img = extract_image(data, offset, pal_data)
-	if img is not None:
-		img.save('fontw33/%d.png' % i)
+  # read the file header (list of offsets)
+  first_offset = struct.unpack_from('<I', data, 0)[0]
+  image_count = first_offset // 4
+  offsets = []
 
+  for i in range(image_count):
+    offsets.append(struct.unpack_from('<I', data, i * 4)[0])
+
+
+  for i, offset in enumerate(offsets):
+    img = extract_image(data, offset, pal_data)
+    if img is not None:
+      img.save(filename + '/%d.png' % i)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
